12889d0dd499fb692995c03ed378fbccf53835af
nit29sin <59767668+nit29sin@users.noreply.github.com>
2020-01-13 20:21:55 +0530

    Update Input and Output.cpp

--- Input and Output.cpp
+++ Input and Output.cpp
@@ -41,3 +41,20 @@ Explanation
 
 The sum of the three numbers is 1+2+7.
 */
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int a,b,c,sum;
+    cin>>a>>b>>c;
+    sum = a+b+c;
+    cout<<sum; 
+    return 0;
+}

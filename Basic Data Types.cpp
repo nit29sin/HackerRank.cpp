ffeebd9356bd4048fa7751d12b3ce1f791a3e7fa
nit29sin <59767668+nit29sin@users.noreply.github.com>
2020-01-13 20:23:56 +0530

    Update Basic Data Types.cpp

--- Basic Data Types.cpp
+++ Basic Data Types.cpp
@@ -47,3 +47,29 @@ a
 334.230
 14049.304930000
 */
+
+
+#include <iostream>
+#include <cstdio>
+#include<iomanip>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+    cin>>a>>b>>c>>d>>e;
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<fixed<<setprecision(3)<<d<<endl;
+    cout<<fixed<<setprecision(9)<<e<<endl;
+
+    return 0;
+}

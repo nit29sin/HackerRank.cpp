954dc3dc924098a534aeb5df0f95239c8c2d2c87
nit29sin <59767668+nit29sin@users.noreply.github.com>
2020-01-14 23:25:36 +0530

    Update Array Introduction.cpp

--- Array Introduction.cpp
+++ Array Introduction.cpp
@@ -30,3 +30,33 @@ Sample Output
 
 2 3 4 1
 */
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n,i;
+    cin>>n;
+    int a[n];
+
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    for(i=n-1;i>=0;i--)
+    {
+        cout<<a[i];
+        cout<<" ";
+   
+    }
+
+    return 0;
+}
